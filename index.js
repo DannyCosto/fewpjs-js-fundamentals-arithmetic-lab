94c5902305d202c7d554ffb3f1fae288a4534d87
Danny <danny.costelloe@gmail.com>
2021-03-15 17:06:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 // Write your code here
+
+let random = Math.floor(Math.random() * 100)
+
+let num3 = 60
+let num4 = 8
+let mod = num3 % num4
+
+let max = Math.max(5, 10, 20)
